77e30b2de963b53d41f9f1defb33a34697ba15d7
npakmuk <Macmep@tutamail.com>
2024-10-10 20:09:44 +0500

    A new class - Texture2D

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -1,5 +1,6 @@
 #include "ResourceManager.h"
 #include "../Renderer/ShaderProgram.h"
+#include "../Renderer/Texture2D.h"
 
 #include <sstream>
 #include <fstream>
@@ -47,7 +48,8 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::loadShaders(const std:
 		return nullptr;
 	}
 	
-	std::shared_ptr<Renderer::ShaderProgram>& newShader = m_shaderPrograms.emplace(shaderName, std::make_shared<Renderer::ShaderProgram>(vertexString, fragmentString)).first->second;
+	std::shared_ptr<Renderer::ShaderProgram>& newShader = m_shaderPrograms.emplace(shaderName, std::make_shared<Renderer::ShaderProgram>(
+																									vertexString, fragmentString)).first->second;
 	if (newShader->isCpmpiled())
 	{
 		return newShader;
@@ -71,7 +73,7 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
 	return nullptr;
 }
  
-void ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath) const
+std::shared_ptr<Renderer::Texture2D> ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath)
 {
 	int chanels = 0;
 	int width = 0;
@@ -81,7 +83,22 @@ void ResourceManager::loadTexture(const std::string& textureName, const std::str
 	if (!pixels)
 	{
 		std::cerr << "Can't load image: " << texturePath << std::endl;
-		return;
+		return nullptr;
 	}
+
+	std::shared_ptr<Renderer::Texture2D> newTexture = m_textures.emplace(textureName, std::make_shared < Renderer::Texture2D>(
+																						width, height, pixels, chanels, GL_LINEAR, GL_CLAMP_TO_EDGE)).first->second;
 	stbi_image_free(pixels);
+	return newTexture;
+}
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::getTexture(const std::string& textureName)
+{
+	TexturesMap::const_iterator it = m_textures.find(textureName);
+	if (it != m_textures.end())
+	{
+		return it->second;
+	}
+	std::cerr << "Can't find the textures: " << textureName << std::endl;
+	return nullptr;
 }
